5a4425fb6f59abb605180f29eeeef525fc3d8d1f
oguzhanet <ogzhncengiz@outlook.com>
2022-02-27 16:14:01 +0300

    27.02.2022

--- MvcWebUI/Controllers/HomeController.cs
+++ MvcWebUI/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace MvcWebUI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        //28
+        //32
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
