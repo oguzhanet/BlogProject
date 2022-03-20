8ab1de035533e2edbb7225020c9b06a65c23d245
oguzhanet <ogzhncengiz@outlook.com>
2022-03-20 17:06:36 +0300

    20.03.2022

---
+++ Business/Conrete/WriterManager.cs
@@ -0,0 +1,26 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Conrete
+{
+    public class WriterManager : IWriterService
+    {
+        private readonly IWriterDal _writerDal;
+
+        public WriterManager(IWriterDal writerDal)
+        {
+            _writerDal = writerDal;
+        }
+
+        public void Add(Writer writer)
+        {
+            _writerDal.Add(writer);
+        }
+    }
+}
---
+++ MvcWebUI/Controllers/RegisterController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcWebUI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        private readonly IWriterService _writerService;
+
+        public RegisterController(IWriterService writerService)
+        {
+            _writerService = writerService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Writer writer)
+        {
+            return View();
+        }
+    }
+}
